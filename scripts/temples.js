d5e699fa89553551f8e5af319d5e0765aa37d3e2
avincer-coder <gar23078@byui.edu>
2024-10-03 11:27:27 -0600

    Temple alubum done

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,18 +2,25 @@ btnNav = document.getElementById("btnHamburger")
 console.log(btnNav)
 deployNav = document.getElementById("navMenu")
 clickCloseNav = document.getElementById("closeNavClick")
-
+mainTag = document.getElementsByTagName("main")[0]
 console.log(clickCloseNav)
 
 btnNav.addEventListener("click", function(){
-    deployNav.style.transform = "translate(0, -40px)" 
-    deployNav.style.marginBottom = 
-    "70px"
+    deployNav.style.top = "0" 
+    mainTag.style.marginTop = "70px"
 })
 
 clickCloseNav.addEventListener("click", function(){
-    deployNav.style.transform = "translate(0, -170px)"
-    deployNav.style.marginBottom =
-    "0"
+    deployNav.style.top = "-200px"
+    mainTag.style.marginTop = "0"
 })
 
+// window seleciona toda la pantalla no viene en elementos porque esta oculto
+window.addEventListener("resize", function(){
+    if(window.innerWidth > 850 ){
+        deployNav.style.top = "0" 
+    }
+    else{
+        mainTag.style.marginTop = "70px"
+    }
+})
